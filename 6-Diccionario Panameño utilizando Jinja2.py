8297ee0b6b34e5f74bfae58531c511475d9bc555
BeatrizGSA <141691086+BeatrizGSA@users.noreply.github.com>
2023-08-20 18:51:49 -0500

    Add files via upload

--- 6-Diccionario Panameño utilizando Jinja2.py
+++ 6-Diccionario Panameño utilizando Jinja2.py
@@ -1,33 +1,49 @@
-from flask import Flask, request, render_template, redirect, url_for
 import redis
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-app = Flask(__name__)
-r = redis.Redis(host='localhost', port=6379, db=0)
+app = FastAPI()
 
-@app.route('/')
-def index():
-    palabras = [(palabra.decode('utf-8'), r.get(palabra).decode('utf-8')) for palabra in r.keys()]
-    return render_template('index.html', palabras=palabras)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.route('/agregar', methods=['POST'])
-def agregar_palabra():
-    palabra = request.form['palabra']
-    significado = request.form['significado']
+redis_url = "redis://default:LApzhSSk8YSwXIshipytd2PIEQutVyv5@redis-18733.c228.us-central1-1.gce.cloud.redislabs.com:18733"
+r = redis.from_url(redis_url)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    palabras = r.keys()
+    palabras_significados = [(palabra.decode('utf-8'), r.get(palabra).decode('utf-8')) for palabra in palabras]
+    return templates.TemplateResponse("index.html", {"request": request, "palabras_significados": palabras_significados})
+
+@app.post("/agregar")
+async def agregar_palabra(palabra: str = Form(...), significado: str = Form(...)):
     r.set(palabra, significado)
-    return redirect(url_for('index'))
+    return {"status": "success"}
 
-@app.route('/editar', methods=['POST'])
-def editar_palabra():
-    palabra = request.form['palabra']
-    nuevo_significado = request.form['nuevo_significado']
-    r.set(palabra, nuevo_significado)
-    return redirect(url_for('index'))
+@app.post("/editar")
+async def editar_palabra(palabra: str = Form(...), nuevo_significado: str = Form(...)):
+    if r.exists(palabra):
+        r.set(palabra, nuevo_significado)
+    return {"status": "success"}
 
-@app.route('/eliminar', methods=['POST'])
-def eliminar_palabra():
-    palabra = request.form['palabra']
-    r.delete(palabra)
-    return redirect(url_for('index'))
+@app.post("/eliminar")
+async def eliminar_palabra(palabra: str = Form(...)):
+    if r.exists(palabra):
+        r.delete(palabra)
+    return {"status": "success"}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.post("/buscar")
+async def buscar_palabra(palabra: str = Form(...)):
+    resultado = r.get(palabra)
+    if resultado:
+        return {"status": "success", "palabra": palabra, "significado": resultado.decode('utf-8')}
+    else:
+        return {"status": "error", "message": "Palabra no encontrada."}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
